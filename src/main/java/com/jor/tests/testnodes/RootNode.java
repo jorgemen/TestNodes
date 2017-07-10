a6ed3f58d59be4f3d82100e4d8accd468f430204
jorge.menendez <jorge.menendez@hpe.com>
2017-07-10 23:24:57 +0200

    firts

---
+++ src/main/java/com/jor/tests/testnodes/RootNode.java
@@ -0,0 +1,18 @@
+package com.jor.tests.testnodes;
+
+import org.netbeans.api.core.ide.ServicesTabNodeRegistration;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+
+/**
+ *
+ * @author jor
+ */
+@ServicesTabNodeRegistration(displayName = )
+public class RootNode extends AbstractNode {
+	
+	public RootNode(Children children) {
+		super(children);
+	}
+	
+}
