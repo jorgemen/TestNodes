0f5b6aba2c4f922ea80c3c6aa6e61ebe31e18e38
jorge.menendez <jorge.menendez@hpe.com>
2017-07-11 01:17:53 +0200

    not working

---
+++ src/main/java/com/jor/tests/testnodes/firstlevel/FirstLevelNode.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jor.tests.testnodes.firstlevel;
+
+import java.beans.IntrospectionException;
+import org.openide.nodes.BeanNode;
+import org.openide.nodes.Children;
+
+/**
+ *
+ * @author jor
+ */
+class FirstLevelNode extends BeanNode<FirstLevelBean> {
+	
+	public FirstLevelNode(FirstLevelBean bean) throws IntrospectionException {
+		super(Children.create(new FirstLevelNodeFactory(),true));
+		setDisplayName(bean.getName());
+	}
+	
+}
